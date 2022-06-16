b1e817c1d75f7a9a8f8b255ada4eb27f75eb33a2
hi-rachelliu <90481924+hi-rachelliu@users.noreply.github.com>
2022-06-16 14:42:56 -0400

    Delete python/Own_Projects directory

--- python/Own_Projects/dicegame.py
+++
@@ -1,8 +0,0 @@
-import random
-
-roll_dice = input( "Do you want to roll a dice?")
-if roll_dice == "Yes" or "Y":
-    dice = random.randint(1, 6)
-    print (dice)
-
-
--- python/Own_Projects/extension_extractor.py
+++
@@ -1,6 +0,0 @@
-fname = input("enter your file name: ")
-position = fname.find ('.') + 1
-print (position)
-print (fname[position:])
-if position == 0: 
-    print("Error: are you sure what you entered has an extension?")
